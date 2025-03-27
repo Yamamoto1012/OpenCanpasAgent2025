5bf9055fd5661f2a0f4bf104f0754b8a0e473419
Yamamoto1012 <pyfahhptzi@i.softbank.jp>
2025-03-27 16:10:52 +0900

    feat: 選択カテゴリの追加

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,10 +3,10 @@ import "./App.css";
 import VRMWrapper from "./features/VRM/VRMWrapper/VRMWrapper";
 import { ChatInterface } from "./features/ChatInterface/ChatInterface";
 import { useState } from "react";
-
-import { Button } from "./components/ui/button";
-import { Info } from "lucide-react";
+import { Info, Volume2 } from "lucide-react";
 import { InfoPanel } from "./features/InfoPanel/InfoPanel";
+import { CategoryButton } from "./features/CategoryButton/CategoryButton";
+import { IconButton } from "./features/IconButton/IconButton";
 
 function App() {
   const [showInfo, setShowInfo] = useState(false);
@@ -31,18 +31,23 @@ function App() {
         </Canvas>
       </div>
 
+      <div className="absolute top-1/11 right-2">
+        <CategoryButton />
+      </div>
       <div className="absolute top-1/11 left-2 p-4 z-10">
         <ChatInterface />
       </div>
-      <div className="absolute top-1/11 right-2 p-4 z-10">
-        <Button
+      <div className="absolute bottom-1/12 right-2 p-4 z-10">
+      <IconButton
+          icon={Info}
           onClick={() => setShowInfo(!showInfo)}
-          variant="outline"
-          size="icon"
-          className="rounded-full w-12 h-12 bg-[#b3cfad] backdrop-blur-md border-white/20 text-white hover:bg-white/20"
-        >
-          <Info className="h-5 w-5" />
-        </Button>
+        />
+      </div>
+      <div className="absolute bottom-2/12 right-2 p-4 z-10">
+      <IconButton
+          icon={Volume2}
+          onClick={() => console.log("音声ボタン押下")}
+        />
       </div>
       {showInfo && <InfoPanel onClose={() => setShowInfo(false)} />}
     </div>
